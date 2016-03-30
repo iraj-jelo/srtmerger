098817ad7a0929d1198d18968fbd166b611b1dec
iraj jelodari <iraj.jelo@gmail.com>
2016-03-31 02:46:03 +0430

    Added files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from main import Merger
+
+## How to use?
+m = Merger(output_file_name="new.srt")
+m.add('./test_srt/en.srt')
+m.add('./test_srt/fa.srt', color="yellow", codec="cp1256")
+m.merge()
